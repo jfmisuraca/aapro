a1a0d5e296787302ab72cf5ad700cd06627978ba
Juan Francisco Misuraca <jfmisuraca@hotmail.com>
2023-02-28 15:17:24 -0300

    Agregada la tarea de la clase 10

---
+++ tarea_clase_10_11/nombre_apellido.js
@@ -0,0 +1,16 @@
+// ejercicio 1
+let nombre = "Francisco";
+let apellido = "Misuraca";
+
+console.log( nombre + " " + apellido );
+
+// ejercicio 2
+let nro1 = 5;
+let nro2 = 32;
+let suma = nro1 + nro2;
+
+console.log(suma);
+
+let otraSuma = suma + 45;
+
+console.log(otraSuma);
